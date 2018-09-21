96b8ee2889bf420f2c570da1a0979bba83d33351
pelev1n <pelevin9911@gmail.com>
2018-09-21 22:54:18 +0300

    Added new activity

---
+++ app/src/main/java/com/arkadygamza/shakedetector/AccelerGeros.java
@@ -0,0 +1,13 @@
+package com.arkadygamza.shakedetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccelerGeros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceler_geros);
+    }
+}
