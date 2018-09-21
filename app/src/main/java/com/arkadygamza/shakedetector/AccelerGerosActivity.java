8d53837aa7d73885ae74b01e205b45bc62b21bcf
pelev1n <pelevin9911@gmail.com>
2018-09-21 23:34:33 +0300

    Added SensorPlotterPrint

--- app/src/main/java/com/arkadygamza/shakedetector/AccelerGerosActivity.java
+++ app/src/main/java/com/arkadygamza/shakedetector/AccelerGerosActivity.java
@@ -1,13 +1,239 @@
 package com.arkadygamza.shakedetector;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class AccelerGerosActivity extends AppCompatActivity {
+import com.jjoe64.graphview.GraphView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.functions.Func2;
+
+public class AccelerGerosActivity extends AppCompatActivity  implements View.OnClickListener{
+
+
+    private final List<SensorPlotterPrint> mPlotters = new ArrayList<>(3);
+
+    private Observable<?> mShakeObservable;
+    private Subscription mShakeSubscription;
+    public String state = "DEFAULT";
+    public Map<String, Double> increaseValue;
+    EditText editValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceler_geros);
+        increaseValue = new HashMap<>();
+        increaseValue.put("X", 0.0);
+        increaseValue.put("Y", 0.0);
+        increaseValue.put("Z", 0.0);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<?> adapter =
+                ArrayAdapter.createFromResource(this, R.array.list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        state = "DEFAULT";
+                        changeState(state);
+                        break;
+
+                    case 1:
+                        state = "X";
+                        changeState(state);
+                        break;
+
+                    case 2:
+                        state = "Y";
+                        changeState(state);
+                        break;
+
+                    case 3:
+                        state = "Z";
+                        changeState(state);
+                        break;
+                    default:
+                        state = "DEFAULT";
+                        changeState(state);
+                        break;
+                }
+                Toast.makeText(getApplicationContext(), i + " + " + l, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        editValue = (EditText) findViewById(R.id.value_edit);
+        Button btnX = (Button) findViewById(R.id.btn_x);
+        Button btnY = (Button) findViewById(R.id.btn_y);
+        Button btnZ = (Button) findViewById(R.id.btn_z);
+        Button btnAll = (Button) findViewById(R.id.btn_all);
+        Button btnCancel = (Button) findViewById(R.id.btn_cancel);
+
+
+        btnX.setOnClickListener(this);
+        btnY.setOnClickListener(this);
+        btnZ.setOnClickListener(this);
+        btnAll.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
+        setupPlotters();
+        mShakeObservable = ShakeDetector.create(this);
+
+        btnX.setEnabled(false);
+        btnY.setEnabled(false);
+        btnZ.setEnabled(false);
+        btnCancel.setEnabled(false);
+        btnAll.setEnabled(false);
+
+        Observable<String> valueObservable = RxEditText.getTextWatcherObservable(editValue);
+        Observable.combineLatest(valueObservable, valueObservable, new Func2<String, String, Boolean>() {
+            @Override
+            public Boolean call(String s, String s2) {
+                if (s.isEmpty() || s2.isEmpty())
+                    return false;
+                else
+                    return true;
+            }
+        }).subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean aBoolean) {
+                btnX.setEnabled(aBoolean);
+                btnY.setEnabled(aBoolean);
+                btnZ.setEnabled(aBoolean);
+                btnCancel.setEnabled(aBoolean);
+                btnAll.setEnabled(aBoolean);
+            }
+        });
     }
+
+    public void updateIncValue(String line, String value) {
+        increaseValue.put(line, Double.valueOf(value));
+        changeIncValue(increaseValue);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.line_gyroscope:
+                state = "gyroscope";
+                Intent intent = new Intent(AccelerGerosActivity.this, GyroscopeActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.line_accelerometr:
+                state = "accelerometr";
+                Intent i = new Intent(AccelerGerosActivity.this, MainActivity.class);
+                startActivity(i);
+                return true;
+
+            case R.id.line_accelerometr_geroscope:
+                return true;
+
+            default:
+                return true;
+        }
+    }
+
+    public void changeState(String state) {
+        mPlotters.get(0).setState(state);
+    }
+
+    public void changeIncValue(Map<String, Double> value) {
+        mPlotters.get(0).setIncValue(value);
+        mPlotters.get(1).setIncValue(value);
+
+    }
+
+    private void setupPlotters() {
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> linearAccSensors = sensorManager.getSensorList(Sensor.TYPE_LINEAR_ACCELERATION);
+        List<Sensor> gyroscopeAccSensors = sensorManager.getSensorList(Sensor.TYPE_GYROSCOPE);
+        mPlotters.add(new SensorPlotterPrint("LIN", (GraphView) findViewById(R.id.graph_accelerometr_both), SensorEventObservableFactory.createSensorEventObservable(linearAccSensors.get(0), sensorManager), state, increaseValue,this));
+        mPlotters.add(new SensorPlotterPrint("GER", (GraphView) findViewById(R.id.graph_geroscope_both), SensorEventObservableFactory.createSensorEventObservable(gyroscopeAccSensors.get(0), sensorManager), state, increaseValue,this));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Observable.from(mPlotters).subscribe(SensorPlotterPrint::onResume);
+        mShakeSubscription = mShakeObservable.subscribe((object) -> Utils.beep());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Observable.from(mPlotters).subscribe(SensorPlotterPrint::onPause);
+        mShakeSubscription.unsubscribe();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_x:
+                updateIncValue("X", editValue.getText().toString());
+                break;
+            case R.id.btn_y:
+                updateIncValue("Y", editValue.getText().toString());
+                break;
+            case R.id.btn_z:
+                updateIncValue("Z", editValue.getText().toString());
+                break;
+            case R.id.btn_all:
+                updateIncValue("X", editValue.getText().toString());
+                updateIncValue("Y", editValue.getText().toString());
+                updateIncValue("Z", editValue.getText().toString());
+                break;
+            case R.id.btn_cancel:
+                updateIncValue("X", "0.0");
+                updateIncValue("Y", "0.0");
+                updateIncValue("Z", "0.0");
+                break;
+        }
+    }
+
+    public void printValueInText(SensorEvent event) {
+
+    }
+
 }
---
+++ app/src/main/java/com/arkadygamza/shakedetector/SensorPlotterPrint.java
@@ -0,0 +1,150 @@
+package com.arkadygamza.shakedetector;
+
+
+import android.graphics.Color;
+import android.hardware.SensorEvent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Map;
+
+import rx.Observable;
+import rx.Subscription;
+
+/**
+ * Draws graph of sensor events
+ */
+public class SensorPlotterPrint {
+    public static final int MAX_DATA_POINTS = 50;
+    public static final int VIEWPORT_SECONDS = 5;
+    public static final int FPS = 10;
+
+    @NonNull
+    private final String mName;
+
+    private final long mStart = System.currentTimeMillis();
+
+    protected final LineGraphSeries<DataPoint> mSeriesXs;
+    protected final LineGraphSeries<DataPoint> mSeriesXf;
+    protected final LineGraphSeries<DataPoint> mSeriesYs;
+    protected final LineGraphSeries<DataPoint> mSeriesYf;
+    protected final LineGraphSeries<DataPoint> mSeriesZs;
+    protected final LineGraphSeries<DataPoint> mSeriesZf;
+    private final Observable<SensorEvent> mSensorEventObservable;
+    private long mLastUpdated = mStart;
+    private Subscription mSubscription;
+    private String state;
+    private Map<String,Double> incValue;
+    private AccelerGerosActivity activity;
+
+    public SensorPlotterPrint(@NonNull String name, @NonNull  GraphView graphView,
+                              @NonNull Observable<SensorEvent> sensorEventObservable, String state, Map<String,Double> incValue,
+                              AccelerGerosActivity view) {
+        this.incValue = incValue;
+        this.state = state;
+        mName = name;
+        mSensorEventObservable = sensorEventObservable;
+        this.activity = view;
+
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setMinX(0);
+        graphView.getViewport().setMaxX(VIEWPORT_SECONDS * 1000); // number of ms in viewport
+
+        graphView.getViewport().setYAxisBoundsManual(true);
+        graphView.getViewport().setMinY(-20);
+        graphView.getViewport().setMaxY(20);
+
+        graphView.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView.getGridLabelRenderer().setVerticalLabelsVisible(false);
+
+        mSeriesXs = new LineGraphSeries<>();
+        mSeriesXf = new LineGraphSeries<>();
+        mSeriesYs = new LineGraphSeries<>();
+        mSeriesYf = new LineGraphSeries<>();
+        mSeriesZs = new LineGraphSeries<>();
+        mSeriesZf = new LineGraphSeries<>();
+
+        mSeriesXs.setColor(Color.RED);
+        mSeriesXf.setColor(Color.YELLOW);
+        mSeriesYs.setColor(Color.GREEN);
+        mSeriesYf.setColor(Color.GRAY);
+        mSeriesZs.setColor(Color.BLUE);
+        mSeriesZf.setColor(Color.CYAN);
+
+        graphView.addSeries(mSeriesXs);
+        graphView.addSeries(mSeriesXf);
+        graphView.addSeries(mSeriesYs);
+        graphView.addSeries(mSeriesYf);
+        graphView.addSeries(mSeriesZs);
+        graphView.addSeries(mSeriesZf);
+    }
+
+
+    public void onResume(){
+        mSubscription = mSensorEventObservable.subscribe(this::onSensorChanged);
+    }
+
+    public void onPause(){
+        mSubscription.unsubscribe();
+    }
+
+    private void onSensorChanged(SensorEvent event) {
+        if (!canUpdateUi()) {
+            return;
+        }
+        switch (state) {
+            case "X":
+                appendData(mSeriesXs, event.values[0]);
+                appendData(mSeriesXf, event.values[0] + incValue.get("X"));
+                activity.printValueInText(event);
+                break;
+            case "Y":
+                appendData(mSeriesYs, event.values[1]);
+                appendData(mSeriesYf, event.values[1] + incValue.get("Y"));
+                activity.printValueInText(event);
+                break;
+            case "Z":
+                appendData(mSeriesZs, event.values[2]);
+                appendData(mSeriesZf, event.values[2] + incValue.get("Z"));
+                activity.printValueInText(event);
+                break;
+            case "DEFAULT":
+                appendData(mSeriesXs, event.values[0]);
+                appendData(mSeriesXf, event.values[0] + incValue.get("X"));
+                appendData(mSeriesYs, event.values[1]);
+                appendData(mSeriesYf, event.values[1] + incValue.get("Y"));
+                appendData(mSeriesZs, event.values[2]);
+                appendData(mSeriesZf, event.values[2] + incValue.get("Z"));
+                activity.printValueInText(event);
+                break;
+        }
+    }
+
+    private boolean canUpdateUi() {
+        long now = System.currentTimeMillis();
+        if (now - mLastUpdated < 1000 / FPS) {
+            return false;
+        }
+        mLastUpdated = now;
+        return true;
+    }
+
+    private void appendData(LineGraphSeries<DataPoint> series, double value) {
+        series.appendData(new DataPoint(getX(), value), true, MAX_DATA_POINTS);
+    }
+
+    public void setState(String s) {
+        this.state = s;
+    }
+
+    public void setIncValue(Map<String,Double> v) {
+        this.incValue = v;
+    }
+    private long getX() {
+        return System.currentTimeMillis() - mStart;
+    }
+}
